beda019c54d2eb9eb75b86cb575da6e1d8fdc23d
HappyRyan2 <happyryan2@hotmail.com>
2021-03-21 17:38:02 -0700

    added Array.partitions method

--- Array.js
+++ Array.js
@@ -5,3 +5,14 @@ Array.method(function repeat(numTimes) {
 	}
 	return result;
 });
+Array.method(function partitions() {
+	/* returns the set of all partitionings of this array. */
+	if(this.length === 1) {
+		return new Set([[this.map(v => v)]]);
+	}
+	const partitionsOfOthers = this.slice(1).partitions();
+	return new Set([
+		...partitionsOfOthers.map(partition => [[this[0]], ...partition]),
+		...partitionsOfOthers.map(partition => [[this[0], ...partition[0]], ...partition.slice(1)])
+	]);
+});
--- tests/Array.js
+++ tests/Array.js
@@ -4,3 +4,20 @@ testing.addUnit("Array.repeat()", [
 		expect(result).toEqual([1, 2, 1, 2, 1, 2]);
 	}
 ]);
+testing.addUnit("Array.partitions()", [
+	() => {
+		const result = [1].partitions();
+		expect(result).toEqual(new Set([
+			[[1]]
+		]));
+	},
+	() => {
+		const result = [1, 2, 3].partitions();
+		expect(result).toEqual(new Set([
+			[[1, 2, 3]],
+			[[1, 2], [3]],
+			[[1], [2, 3]],
+			[[1], [2], [3]]
+		]));
+	}
+]);
