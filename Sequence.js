64bf661623bd59483ec08390afde1120810e96ec
HappyRyan2 <happyryan2@hotmail.com>
2021-04-09 11:25:23 -0700

    added built-in sequences for integers, positive integers, and primes

--- Sequence.js
+++ Sequence.js
@@ -8,4 +8,45 @@ class Sequence {
 		};
 		this[Symbol.iterator] = generator;
 	}
+
+	static POSITIVE_INTEGERS = new Sequence(
+		function*() {
+			for(let i = 1; i < Infinity; i ++) {
+				yield i;
+			}
+		},
+		{ isMonotonic: true }
+	);
+	static INTEGERS = new Sequence(
+		function*() {
+			yield 0;
+			for(let i = 1; i < Infinity; i ++) {
+				yield i;
+				yield -i;
+			}
+		},
+		{ isMonotonic: false }
+	);
+	static PRIMES = new Sequence(
+		function*() {
+			const nextPrimeDivisors = []; // the `i`th sub-array contains the distinct prime factors of `i`.
+			for(let i = 2; i < Infinity; i ++) {
+				const distinctFactors = nextPrimeDivisors[i] ?? [];
+				if(distinctFactors.length === 0) {
+					/* i is prime */
+					yield i;
+					nextPrimeDivisors[i * 2] ??= [];
+					nextPrimeDivisors[i * 2].push(i);
+				}
+				else {
+					for(const factor of distinctFactors) {
+						nextPrimeDivisors[i + factor] ??= [];
+						nextPrimeDivisors[i + factor].push(factor);
+					}
+				}
+				delete nextPrimeDivisors[i];
+			}
+		},
+		{ isMonotonic: true }
+	);
 }
--- tests/Sequence.js
+++ tests/Sequence.js
@@ -11,3 +11,34 @@ testing.addUnit("Sequence iteration", [
 		expect(sequenceItems).toEqual([1, 2, 3, 4, 5]);
 	}
 ]);
+
+testing.addUnit("Sequence.POSITIVE_INTEGERS", [
+	() => {
+		const integers = [];
+		for(const integer of Sequence.POSITIVE_INTEGERS) {
+			integers.push(integer);
+			if(integers.length >= 9) { break; }
+		}
+		expect(integers).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+	}
+]);
+testing.addUnit("Sequence.INTEGERS", [
+	() => {
+		const integers = [];
+		for(const integer of Sequence.INTEGERS) {
+			integers.push(integer);
+			if(integers.length >= 9) { break; }
+		}
+		expect(integers).toEqual([0, 1, -1, 2, -2, 3, -3, 4, -4]);
+	}
+]);
+testing.addUnit("Sequence.PRIMES", [
+	() => {
+		const primes = [];
+		for(const prime of Sequence.PRIMES) {
+			primes.push(prime);
+			if(primes.length >= 10) { break; }
+		}
+		expect(primes).toEqual([2, 3, 5, 7, 11, 13, 17, 19, 23, 29]);
+	}
+]);
